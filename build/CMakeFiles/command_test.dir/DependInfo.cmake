c7667c35af5b04322d2bae3280e4bc045b578c42
AngelinaScheck <angelina.scheck@outlook.com>
2016-05-09 18:54:31 +0200

    example build with expression files in build directory

---
+++ build/CMakeFiles/command_test.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/norbert/workspace/off-target-effects_Seqan/Coding/github/command_test.cpp" "/home/norbert/workspace/off-target-effects_Seqan/Coding/github/build/CMakeFiles/command_test.dir/command_test.cpp.o"
+  "/home/norbert/workspace/off-target-effects_Seqan/Coding/github/inputTransc.cpp" "/home/norbert/workspace/off-target-effects_Seqan/Coding/github/build/CMakeFiles/command_test.dir/inputTransc.cpp.o"
+  "/home/norbert/workspace/off-target-effects_Seqan/Coding/github/parse.cpp" "/home/norbert/workspace/off-target-effects_Seqan/Coding/github/build/CMakeFiles/command_test.dir/parse.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "SEQAN_HAS_EXECINFO=1"
+  "SEQAN_HAS_ZLIB=1"
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE_SOURCE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/command_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/command_test.dir/command_test.cpp.o"
+  "CMakeFiles/command_test.dir/parse.cpp.o"
+  "CMakeFiles/command_test.dir/inputTransc.cpp.o"
+  "command_test.pdb"
+  "command_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/command_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
