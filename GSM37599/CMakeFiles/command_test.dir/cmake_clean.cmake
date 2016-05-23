8e20412befe5d2a50cb8a53fa2e76afce1f89517
AngelinaScheck <angelina.scheck@outlook.com>
2016-05-23 11:24:47 +0200

    Performance Test

---
+++ GSM37599/CMakeFiles/command_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/command_test.dir/command_test.cpp.o"
+  "CMakeFiles/command_test.dir/parse.cpp.o"
+  "CMakeFiles/command_test.dir/inputTransc.cpp.o"
+  "CMakeFiles/command_test.dir/kmers.cpp.o"
+  "CMakeFiles/command_test.dir/countFinds.cpp.o"
+  "CMakeFiles/command_test.dir/fisher.cpp.o"
+  "command_test.pdb"
+  "command_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/command_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
